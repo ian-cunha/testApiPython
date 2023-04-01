52229c246878d72faf43295cc5b69385b67162ae
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-04-01 02:09:46 -0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Bem-vindo à minha API!'
+
+
+@app.route('/usuarios')
+def get_usuarios():
+    # código para buscar e retornar dados de usuários
+    return 'Dados dos usuários'
+
+
+@app.route('/usuarios', methods=['POST'])
+def criar_usuario():
+    # código para criar um novo usuário
+    return 'Novo usuário criado'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
